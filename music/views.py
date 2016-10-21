823ac5e0a0c941ca0d8b62984bb10e0cebac5282
Shubham Goel <shubham145@hotmail.com>
2016-10-21 17:32:46 +0100

    Connected database to show all albums as links, redirecting them their own custom url or thier ID

--- music/views.py
+++ music/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1> Music Home </h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href ="' + url + '">' + album.album_title + '</a><br>'
+
+    return HttpResponse(html)
+
+
+
+def detail(request, album_id):
+        return HttpResponse("<h2>Details For Album ID:" + str(album_id) +"</h2>")
+
